4af23612d33b3f9709274e7dbb613ca138a8d3e0
pehzhengda <23023133@mrp.edu.sg>
2025-02-05 10:48:00 +0800

    RecipeDetails

---
+++ frontend/src/components/RecipeDetails.js
@@ -0,0 +1,43 @@
+import { useRecipeContext } from '../hooks/useRecipeContext'
+import { useAuthContext } from '../hooks/useAuthContext'
+
+// date fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+const RecipeDetails = ({ recipe }) => {
+  const { dispatch } = useRecipeContext()
+  const { user } = useAuthContext()
+
+  const handleClick = async () => {
+    if (!user) {
+      return
+    }
+
+    const response = await fetch('/api/recipes' + recipe._id, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      }
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      dispatch({type: 'DELETE_RECIPE', payload: json})
+    }
+  }
+
+  return (
+    <div className="recipe-details">
+      <h4>{recipe.title}</h4>
+      <p><strong>Recipe name: </strong>{recipe.name}</p>
+      <p><strong>Ingrediants: </strong>{recipe.ingrediants}</p>
+      <p><strong>Cooking instructions: </strong>{recipe.instructions}</p>
+      <p><strong>Preperation time: </strong>{recipe.time}</p>
+      <p><strong>Difficulty Level: </strong>{recipe.level}</p>
+      <p>{formatDistanceToNow(new Date(recipe.createdAt), { addSuffix: true })}</p>
+      <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+    </div>
+  )
+}
+
+export default RecipeDetails
