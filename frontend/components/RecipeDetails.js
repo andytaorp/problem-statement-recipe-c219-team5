123666dc961b7bc4265bb853e99c174fa41f3905
pehzhengda <23023133@mrp.edu.sg>
2025-02-05 10:23:56 +0800

    recipedetail

---
+++ frontend/components/RecipeDetails.js
@@ -0,0 +1,39 @@
+import { useWorkoutsContext } from '../hooks/useWorkoutContext'
+import { useAuthContext } from '../hooks/useAuthContext'
+
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+const RecipeDetails = ({ recipe }) => {
+  const { dispatch } = useWorkoutsContext()
+  const { user } = useAuthContext()
+
+  const handleClick = async () => {
+    if (!user) {
+      return
+    }
+
+    const response = await fetch(`${process.env.REACT_APP_API_URL}api/recipes` + recipe._id, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      }
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      dispatch({type: 'DELETE_RECIPE', payload: json})
+    }
+  }
+
+  return (
+    <div className="recipe-details">
+      <h4>{recipe.title}</h4>
+      <p><strong>Load (kg): </strong>{recipe.load}</p>
+      <p><strong>Reps: </strong>{recipe.reps}</p>
+      <p>{formatDistanceToNow(new Date(recipe.createdAt), { addSuffix: true })}</p>
+      <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+    </div>
+  )
+}
+
+export default RecipeDetails
