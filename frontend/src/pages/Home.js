9e32682dff91ee63120423113dc70f7984587e9f
BlackNugget <23023898@myrp.edu.sg>
2025-02-05 10:25:58 +0800

    completed Home.js 36 lines of code

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,3 +1,36 @@
 import {useEffect} from 'react'
 
-const Home = () =>
+const Home = () => {
+    const {recipes, dispatch} = useRecipeContext()
+    const {user} = useAuthContext()
+
+    useEffect(() => {
+        const fetchRecipes = async() => {
+            const response = await fetch('/api/recipes', {
+                headers: {'Authorization' : `Bearer ${user.token}`},
+            })
+            const json = await response.json()
+
+      if (response.ok) {
+        dispatch({type: 'SET_RECIPES', payload: json})
+      }
+    }
+
+    if (user) {
+      fetchRecipes()
+    }
+  }, [dispatch, user])
+
+  return (
+    <div className="home">
+      <div className="recipes">
+        {recipes && recipes.map((recipe) => (
+          <RecipeDetails key={recipe._id} recipe={recipe} />
+        ))}
+      </div>
+      <RecipeForm />
+    </div>
+  )
+}
+
+export default Home
