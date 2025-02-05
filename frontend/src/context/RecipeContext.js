6bdebb64809ddd89781f63398b235e1377a482d2
liam <23021630@myrp.edu.sg>
2025-02-05 16:11:44 +0800

    sort

--- frontend/src/context/RecipeContext.js
+++ frontend/src/context/RecipeContext.js
@@ -1,40 +1,50 @@
-import { createContext, useReducer } from 'react'
+import { createContext, useReducer } from 'react';
 
-export const RecipeContext = createContext()
+export const RecipeContext = createContext();
 
 export const recipesReducer = (state, action) => {
   switch (action.type) {
     case 'SET_RECIPE': 
       return {
+        ...state,
         recipes: action.payload
-      }
+      };
     case 'CREATE_RECIPE':
       return {
+        ...state,
         recipes: [action.payload, ...state.recipes]
-      }
+      };
     case 'DELETE_RECIPE':
       return {
+        ...state,
         recipes: state.recipes.filter((r) => r._id !== action.payload._id)
-      }
+      };
     case 'UPDATE_RECIPE':
       return {
+        ...state,
         recipes: state.recipes.map((recipe) =>
-        recipe._id === action.payload._id ? { ...recipe, ...action.payload } : recipe
-    ),
-  }
+          recipe._id === action.payload._id ? { ...recipe, ...action.payload } : recipe
+        ),
+      };
+    case 'SET_SORT_ORDER':
+      return {
+        ...state,
+        sortOrder: action.payload
+      };
     default:
-      return state
+      return state;
   }
-}
+};
 
 export const RecipeContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(recipesReducer, {
-    recipes: [] 
-  })
+    recipes: [],
+    sortOrder: 'none' 
+  });
 
   return (
-    <RecipeContext.Provider value={{...state, dispatch}}>
+    <RecipeContext.Provider value={{ ...state, dispatch }}>
       {children}
     </RecipeContext.Provider>
-  )
-}
+  );
+};
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,39 +1,66 @@
-import {useEffect} from 'react'
-import { useRecipeContext } from "../hooks/useRecipeContext"
-import { useAuthContext } from "../hooks/useAuthContext"
-import RecipeDetails from '../components/RecipeDetails'
-import RecipeForm from '../components/RecipeForm'
+import { useEffect, useState } from 'react';
+import { useRecipeContext } from '../hooks/useRecipeContext';
+import { useAuthContext } from '../hooks/useAuthContext';
+import RecipeDetails from '../components/RecipeDetails';
+import RecipeForm from '../components/RecipeForm';
+
 const Home = () => {
-    const {recipes, dispatch} = useRecipeContext()
-    const {user} = useAuthContext()
+    const { recipes, dispatch } = useRecipeContext();
+    const { user } = useAuthContext();
+    const [sortOrder, setSortOrder] = useState('none'); 
 
     useEffect(() => {
-        const fetchRecipes = async() => {
-            const response = await fetch('http://localhost:4000/api/recipes', {
-                headers: {'Authorization' : `Bearer ${user.token}`},
-            })
-            const json = await response.json()
+        const fetchRecipes = async () => {
+            const response = await fetch('/api/recipes', {
+                headers: { 'Authorization': `Bearer ${user.token}` },
+            });
+            const json = await response.json();
 
-      if (response.ok) {
-        dispatch({type: 'SET_RECIPES', payload: json})
-      }
-    }
+            if (response.ok) {
+                dispatch({ type: 'SET_RECIPES', payload: json });
+            }
+        };
 
-    if (user) {
-      fetchRecipes()
-    }
-  }, [dispatch, user])
+        if (user) {
+            fetchRecipes();
+        }
+    }, [dispatch, user]);
 
-  return (
-    <div className="home">
-      <div className="recipes">
-        {recipes && recipes.map((recipe) => (
-          <RecipeDetails key={recipe._id} recipe={recipe} />
-        ))}
-      </div>
-      <RecipeForm />
-    </div>
-  )
-}
+    
+    const sortedRecipes = () => {
+        if (!recipes) return [];
 
-export default Home
+        let sorted = [...recipes];
+
+        if (sortOrder === 'asc') {
+            sorted.sort((a, b) => a.preparationTime - b.preparationTime);
+        } else if (sortOrder === 'desc') {
+            sorted.sort((a, b) => b.preparationTime - a.preparationTime);
+        }
+
+        return sorted;
+    };
+
+    return (
+        <div className="home">
+            <div className="sorting-controls">
+                <label>Sort by Preparation Time:</label>
+                <select value={sortOrder} onChange={(e) => setSortOrder(e.target.value)}>
+                    <option value="none">None</option>
+                    <option value="asc">Ascending</option>
+                    <option value="desc">Descending</option>
+                </select>
+            </div>
+
+            <div className="recipes">
+                {sortedRecipes().map((recipe) => (
+                    <RecipeDetails key={recipe._id} recipe={recipe} />
+                ))}
+            </div>
+
+            <RecipeForm />
+        </div>
+    );
+};
+
+export default Home;
