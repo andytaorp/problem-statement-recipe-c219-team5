246ee002ee1fae4e82675d32bec282d0fb2d66f3
Ngweijun23021523 <23021523@myrp.edu.sg>
2025-02-05 10:37:30 +0800

    Added Recipe Context

---
+++ frontend/src/context/RecipeContext.js
@@ -0,0 +1,40 @@
+import { createContext, useReducer } from 'react'
+
+export const WorkoutsContext = createContext()
+
+export const workoutsReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_RECIPE': 
+      return {
+        recipe: action.payload
+      }
+    case 'CREATE_RECIPE':
+      return {
+        recipe: [action.payload, ...state.recipe]
+      }
+    case 'UPDATE_RECIPE':
+      return {
+        recipe: state.recipe.map((w) => 
+          w._id === action.payload._id ? action.payload : w
+        )
+      }
+    case 'DELETE_RECIPE':
+      return {
+        recipe: state.recipe.filter((w) => w._id !== action.payload._id)
+      }
+    default:
+      return state
+  }
+}
+
+export const RecipeContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(workoutsReducer, {
+    recipe: null
+  })
+
+  return (
+    <WorkoutsContext.Provider value={{...state, dispatch}}>
+      { children }
+    </WorkoutsContext.Provider>
+  )
+}
